b018ab2d24f81de87687ca6d0d4f66d0691e8edf
Md.Mahbub Alam <mmahbub569@gmail.com>
2020-07-12 11:55:18 +0600

    new commits

--- static/js/cart.js
+++ static/js/cart.js
@@ -5,11 +5,36 @@ for(var i = 0;i<updateBtns.length;i++){
         var action = this.dataset.action
 
         if(user === 'AnonymousUser')
-         console.log('User not logged in!')
+            addCookieItem(productId,action)
         else{
             updateUserOrder(productId,action)
         }
     });
+}
+
+
+function addCookieItem(productId,action){
+    console.log('User not logged in!')
+    if(action == 'add'){
+        if(cart[productId]== undefined){
+            cart[productId]={'quantity':1}
+        }
+        else{
+            cart[productId]['quantity']+=1
+        }
+    }
+
+    if(action == 'remove'){
+        cart[productId]['quantity']-=1
+
+        if(cart[productId]['quantity']<=0){
+            console.log('Remove item')
+            delete cart[productId]
+        }   
+    }
+console.log('Cart ',cart)
+document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/"
+location.reload()
 }
 
 function updateUserOrder(productId, action){
--- store/urls.py
+++ store/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('cart/',views.cart, name='cart'),
     path('checkout/',views.checkout, name='checkout'),
     path('updateItem/',views.updateItem, name='updateItem'),
+    path('processOrder/',views.processOrder, name='processOrder'),
     
 ]
